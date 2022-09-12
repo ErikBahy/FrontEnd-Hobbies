d927a5a5d724e4143c7d631b7e9a0f926159e6c3
ErikBahy <66439818+ErikBahy@users.noreply.github.com>
2022-09-12 12:35:47 +0200

    changes

--- src/Components/Feed.jsx
+++ src/Components/Feed.jsx
@@ -3,23 +3,18 @@ import React from "react";
 import Post from "./Post";
 
 function Feed() {
-  let a=[]
-  for(let i=0;i<100;i++){
-    a.push(i)
+  let a = [];
+  for (let i = 0; i < 100; i++) {
+    a.push(i);
   }
   return (
     <>
       <Stack flexDirection="row">
         <Box flex={2} sx={{ display: { xs: "none", sm: "block" } }}></Box>
         <Box flex={4}>
-          {
-            a.map((el)=>{
-              return(
-                <Post />
-              )
-            })
-          }
-
+          {a.map((el) => {
+            return <Post />;
+          })}
         </Box>
         <Box flex={2} sx={{ display: { xs: "none", sm: "block" } }}></Box>
       </Stack>
---
+++ src/Components/contexts/UserContext.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { createContext } from "react";
+
+const ProductContext = createContext();
+
+function UserContext() {
+  return <div></div>;
+}
+
+export default UserContext;
