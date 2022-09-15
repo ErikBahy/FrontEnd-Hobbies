eaf0ab7efe6739c49a3943e4e8d13a434e9a71a7
ErikBahy <66439818+ErikBahy@users.noreply.github.com>
2022-09-15 15:31:58 +0200

    navbar feed context

--- src/Components/Feed.jsx
+++ src/Components/Feed.jsx
@@ -8,19 +8,20 @@ const url = "https://0tcdj2tfi8.execute-api.eu-central-1.amazonaws.com";
 
 function Feed({ called }) {
   const [posts, setPosts] = useState([]);
-  const [pageNumber,setPageNumber]= useState(1);
-  
+  const [pageNumber, setPageNumber] = useState(1);
+
   const getAllPosts = async () => {
     //const res = await axios.get(`${url}/dev/allposts`);
-    const res = await axios.get(`https://0tcdj2tfi8.execute-api.eu-central-1.amazonaws.com/dev/allposts?page=${pageNumber}`);
+    const res = await axios.get(
+      `https://0tcdj2tfi8.execute-api.eu-central-1.amazonaws.com/dev/allposts?page=${pageNumber}`
+    );
     const data = res.data;
 
-    let i = []
-    data.post.map((el)=> i.push(el)  )
-    
-    
-    setPosts(i)
-    console.log( posts , " state from feed");
+    let i = [];
+    data.post.map((el) => i.push(el));
+
+    setPosts(i);
+    console.log(posts, " state from feed");
   };
 
   useEffect(() => {
@@ -37,9 +38,7 @@ function Feed({ called }) {
         <Box flex={2} sx={{ display: { xs: "none", sm: "block" } }}></Box>
         <Box flex={4}>
           {posts.map((el) => {
-            return (
-              <Post  post={el}/>
-              );
+            return <Post post={el} />;
           })}
         </Box>
         <Box flex={2} sx={{ display: { xs: "none", sm: "block" } }}></Box>
