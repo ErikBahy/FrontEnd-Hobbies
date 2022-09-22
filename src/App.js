cdbb240500c84b1d1d860c81a9075529cf9e7f79
Arbs28 <87767366+Arbs28@users.noreply.github.com>
2022-09-22 16:44:25 +0200

    log in bug

--- src/App.js
+++ src/App.js
@@ -39,11 +39,11 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={!isLogged ? <Login /> : <Navigate replace to={"/mainpage"} />} />
-        <Route path="/mainPage" element={!isLogged ? <MainPage /> : <Navigate replace to={"/"} />} />
-        <Route path="/newpost" element={isLogged ? <NewPostModalNewPage /> : <Navigate replace to={"/"} />} />
-        <Route path="/editprofile" element={isLogged ? <EditProfile /> : <Navigate replace to={"/"} />} />
-        <Route path="/editmodal" element={isLogged ? <EditProfileModal /> : <Navigate replace to={"/"} />}/>
+        <Route path="/" element={!isLogged ? <Login /> : <Navigate replace to={"/mainPage"} />} />
+        <Route path="/mainPage" element={ <MainPage />} />
+        <Route path="/newpost" element={<NewPostModalNewPage />} />
+        <Route path="/editprofile" element={<EditProfile/>} />
+        <Route path="/editmodal" element={<EditProfileModal/>}/>
         <Route path="/userprofile/:cognitoId" element={ <UserProfile />}/>
       </Routes>
     </BrowserRouter>
