b0c2e6a4de505c6923093c2147fb2caee11197f9
ErikBahy <66439818+ErikBahy@users.noreply.github.com>
2022-09-22 14:04:41 +0200

    feed loaders

--- src/Components/Feed.jsx
+++ src/Components/Feed.jsx
@@ -27,6 +27,25 @@ function Feed({ called, setTag, tag, effectRunFromModal }) {
   const matchesDesktop = useMediaQuery(theme.breakpoints.up("sm"));
 
   const getAllPosts = async () => {
+    try {
+      const page = await axios.get(
+        `https://0tcdj2tfi8.execute-api.eu-central-1.amazonaws.com/dev/allpostsPages`
+      );
+      setTotalPages(parseInt(page.data));
+      const res = await axios.get(
+        `https://0tcdj2tfi8.execute-api.eu-central-1.amazonaws.com/dev/allposts?page=${pageNumber}`
+      );
+      const data = res.data;
+
+      let i = [];
+      data.post.map((el) => i.push(el));
+
+      setPosts(i);
+      console.log(posts, " state from feed");
+      setloading(false);
+    } catch (error) {
+      console.log(error);
+    }
     const page = await axios.get(
       `https://0tcdj2tfi8.execute-api.eu-central-1.amazonaws.com/dev/allpostsPages`
     );
@@ -44,44 +63,47 @@ function Feed({ called, setTag, tag, effectRunFromModal }) {
   };
 
   const getPostsByTag = async () => {
-    if (tag.length === 1) {
-      const page = await axios.get(
-        `https://0tcdj2tfi8.execute-api.eu-central-1.amazonaws.com/dev/allPagesforLS?tags=${tag}`
-      );
-      setTotalPages(parseInt(page.data));
-      const res = await axios.get(
-        `https://0tcdj2tfi8.execute-api.eu-central-1.amazonaws.com/dev/bylocationsport?page=${pageNumber}&tags=${tag}`
-      );
-      const data = res.data;
-      console.log(data);
-      let i = [];
-      data.map((el) => i.push(el));
+    try {
+      setloading(true);
+      if (tag.length === 1) {
+        const page = await axios.get(
+          `https://0tcdj2tfi8.execute-api.eu-central-1.amazonaws.com/dev/allPagesforLS?tags=${tag}`
+        );
+        setTotalPages(parseInt(page.data));
+        const res = await axios.get(
+          `https://0tcdj2tfi8.execute-api.eu-central-1.amazonaws.com/dev/bylocationsport?page=${pageNumber}&tags=${tag}`
+        );
+        const data = res.data;
+        console.log(data);
+        let i = [];
+        data.map((el) => i.push(el));
 
-      setPosts(i);
-    } else if (tag.length > 1) {
-      const page = await axios.get(
-        `https://0tcdj2tfi8.execute-api.eu-central-1.amazonaws.com/dev/allPagesforTags?tags=${tag}`
-      );
-      setTotalPages(parseInt(page.data));
-      let str = tag.join();
-      console.log(str);
+        setPosts(i);
+      } else if (tag.length > 1) {
+        const page = await axios.get(
+          `https://0tcdj2tfi8.execute-api.eu-central-1.amazonaws.com/dev/allPagesforTags?tags=${tag}`
+        );
+        setTotalPages(parseInt(page.data));
+        let str = tag.join();
+        console.log(str);
 
-      const res = await axios.get(
-        `https://0tcdj2tfi8.execute-api.eu-central-1.amazonaws.com/dev/bytag?page=${pageNumber}&tags=${str}`
-      );
-      const data = res.data;
-      console.log(data);
-      let i = [];
-      data.map((el) => i.push(el));
+        const res = await axios.get(
+          `https://0tcdj2tfi8.execute-api.eu-central-1.amazonaws.com/dev/bytag?page=${pageNumber}&tags=${str}`
+        );
+        const data = res.data;
+        console.log(data);
+        let i = [];
+        data.map((el) => i.push(el));
 
-      setPosts(i);
-    } else {
-      shouldEffectRun ? setshouldEffectRun(false) : setshouldEffectRun(true);
+        setPosts(i);
+      } else {
+        shouldEffectRun ? setshouldEffectRun(false) : setshouldEffectRun(true);
+      }
+      setloading(false);
+    } catch (error) {
+      console.log(error);
     }
   };
-  setTimeout(() => {
-    setloading(false);
-  }, [3000]);
 
   useEffect(() => {
     getPostsByTag();
