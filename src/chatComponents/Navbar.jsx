0f2e9fab76de9f1d4688e01c6197731c6ea3afd2
Fane Veizi <faneveizi@gmail.com>
2022-09-27 16:56:07 +0200

    chat-logo

--- src/chatComponents/Navbar.jsx
+++ src/chatComponents/Navbar.jsx
@@ -7,7 +7,7 @@ import React from 'react';
 const Navbar = () => {
   return (
     <div className="nav-bar-chat">
-        <img src='/Sport-chat.png' alt="" height={200} width={180} style={{paddingTop: 12,paddingLeft: 20, 
+        <img src='/hobby-chat.png' alt="" height={200} width={160} style={{paddingTop: 12,paddingLeft: 20, 
                                                                       paddingBottom: 12, paddingRight: 20}} />
         {/* <h1 className={style.heading}>Chat App</h1> */}
     </div>
