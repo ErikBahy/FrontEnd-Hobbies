0556ff0daaf713e22d294f7533866d25125ea0fc
Fane Veizi <faneveizi@gmail.com>
2022-09-29 12:31:59 +0200

    name margin

--- src/chatComponents/Message.jsx
+++ src/chatComponents/Message.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const style = {
   message: `flex break-all items-center m-4 py-2 px-3 `,
-  name: `absolute mt-[-4rem] text-gray-600 text-xs overflow-auto mb-[22px] mr-[10px] ml-[30px]`,
+  name: `absolute mt-[-4rem] text-gray-600 text-xs overflow-auto mb-[20px] mr-[7px] ml-[7px]`,
   sent: `bg-[#118C94] text-white flex-row-reverse text-end float-right`,
   received: `bg-[#e5e5ea] text-black float-left`,
 };
