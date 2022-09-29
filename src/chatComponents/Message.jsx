27276dda836519ce4d03e9b2cf093cc15eeb106b
Fane Veizi <faneveizi@gmail.com>
2022-09-29 12:28:55 +0200

    name

--- src/chatComponents/Message.jsx
+++ src/chatComponents/Message.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const style = {
   message: `flex break-all items-center m-4 py-2 px-3 `,
-  name: `relative mt-[-4rem] text-gray-600 text-xs overflow-auto mb-[5px] mr-[10px]`,
+  name: `absolute mt-[-4rem] text-gray-600 text-xs overflow-auto mb-[22px] mr-[10px] ml-[30px]`,
   sent: `bg-[#118C94] text-white flex-row-reverse text-end float-right`,
   received: `bg-[#e5e5ea] text-black float-left`,
 };
